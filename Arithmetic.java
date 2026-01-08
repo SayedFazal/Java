cbc686410f1693564dd6d947f35f225900883ccc
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-08 12:06:53 +0530

    main

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+class R192311291{
+  public static void main (String[] args)
+  {
+    int a = 5;
+    int b = 5;
+    System.out.println("Sum = " + (a+b)); 
+    System.out.println("Difference = " + (a-b)); 
+    System.out.println("Product = " + (a*b)); 
+    System.out.println("Quoitent = " + (a/b)); 
+  }
+}
