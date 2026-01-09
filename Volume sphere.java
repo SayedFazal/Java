ccb10432ff48ebedca445fc1cb5c0b3a61b1f7ec
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-09 13:16:46 +0530

    main

---
+++ Volume sphere.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class R192311291{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    double r = sc.nextDouble();
+    double volume = (4.0 / 3.0) * 3.14 * r * r * r;
+    System.out.println(volume);
+    }
+}
