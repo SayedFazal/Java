46c2019a66105dd4d6def11f42ace58bfb599cf8
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-09 13:24:12 +0530

    main

---
+++ Perfect Number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class R192311291{
+  public static void main(String[] args){
+    int num = 6;
+    int sum = 0;
+    for(int i=1;i<num;i++){
+      if(num%i==0){
+        sum+=i;
+      }
+    }
+    if(num==sum){
+      System.out.print("Perfect number!");
+    }
+    else{
+      System.out.print("Not perfect number!");
+    }
+  }
+}
