b8c7f9a12cd06994819e59cb5f6b7761f58b3572
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-09 13:22:59 +0530

    main

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class R192311291{
+  public static void main(String[] args){
+    String str = "POP";
+    boolean flag = true;
+    for (int i = 0; i < str.length() / 2; i++){
+      if (str.charAt(i) != str.charAt(str.length() - i - 1)){
+        flag = false;
+        break;
+      }
+    }
+    if (flag)
+      System.out.println("Palindrome");
+    else
+      System.out.println("Not Palindrome");
+  }
+}
