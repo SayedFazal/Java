c3e83895ff935c2df719d4d1684dca747ac799cc
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-19 13:31:08 +0530

    main

---
+++ Hollow Square.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class R192311291{
+  public static void main(String[] args){
+    Scanner sc= new Scanner(System.in);
+    int n= sc.nextInt();
+    for(int i=0; i<n; i++){
+      for(int j=0; j<n; j++){
+        if(i==0 || j==0 || i==4 || j==4){
+          System.out.print("* ");
+        }
+        else{
+          System.out.print("  ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
