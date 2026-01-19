6af29ac108f723d4ea8b5c71ab07233b55003ce9
Fazal Sayed <syedfazal2956@gmail.com>
2026-01-19 09:58:37 +0530

    main

---
+++ Simplesort.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class R192311291 {
+    public static void main(String[] args) {
+      int num[]= {4,2,5,3,6,9,10,1};
+      Arrays.sort(num);
+      System.out.print("Sorted array:"+ Arrays.toString(num));
+    }
+}
